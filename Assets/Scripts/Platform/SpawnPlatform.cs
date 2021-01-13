ef8c901089827609dd4e405f06a4c30044410cfd
nelya07 <75816118+nelya07@users.noreply.github.com>
2021-01-13 18:24:35 +0300

    Helix Jump

---
+++ Assets/Scripts/Platform/SpawnPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : Platform
+{
+    [SerializeField] private Ball _ball;
+    [SerializeField] private Transform _spawnPoint;
+
+
+    private void Awake()
+    {
+        Instantiate(_ball, _spawnPoint.position, Quaternion.identity);
+    }
+}
