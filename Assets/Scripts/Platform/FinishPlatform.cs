f930f53b2cfda54fa9c81c4ada6be972d2b8d1fb
nelya07 <75816118+nelya07@users.noreply.github.com>
2021-01-26 18:10:05 +0300

    Helix Jump

--- Assets/Scripts/Platform/FinishPlatform.cs
+++ Assets/Scripts/Platform/FinishPlatform.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 
 public class FinishPlatform : Platform
 {
+    private bool _isGameFinished = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_isGameFinished && collision.gameObject.TryGetComponent(out Ball _ball))
+        {
+            _isGameFinished = true;
+            Debug.Log("Congratulations! You are winner! ${name}");
+        }
+    }
 }
 
